e17178ea25ce6ee7394197bd3d6411423f45327b
Erick <erick@corteperfeito.com>
2026-03-31 21:49:03 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/barbearia-o-corte-perfeito/sw.js', { scope: '/barbearia-o-corte-perfeito/' })})}
